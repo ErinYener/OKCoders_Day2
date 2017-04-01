a0c96efa58a049581d6073afbf54aefcfe735c3b
Erin Jones <Erin Jones>
2017-04-01 16:38:43 +0100

    Bomber's future

---
+++ taco.js
@@ -0,0 +1,15 @@
+var name = "Bomber"
+var age = 5
+var mammal = "cat"
+
+console.log("My name is " + name + " and I am " + age + ".");
+
+var drinkingAge = 21-age
+
+if (age < 21) {
+  console.log("I can have a beer in " + drinkingAge + " years.");
+} else if (mammal != "human") {
+  console.log ("But cats can't drink.");
+} else {
+  console.log ("Wait...I'm a cat.")
+}
